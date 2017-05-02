b76a35b46fcabafd69d618ffd2df1a68cbb66bc5
BatyaHack <officalbatya@gmail.com>
2017-05-02 12:39:53 +0300

    добавил одиночную рассылку

--- config/web.php
+++ config/web.php
@@ -28,10 +28,16 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'viewPath' => '@app/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'vasa11514@gmail.com',
+                'password' => 'qwertyzxcvb',
+                'port' => 465,
+                'encryption' => 'ssl',
+            ],
+            'useFileTransport' => false,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
--- controllers/SaveemailController.php
+++ controllers/SaveemailController.php
@@ -16,12 +16,20 @@ class SaveemailController extends Controller
 
         $request = Yii::$app->request;
         $get = $request->get();
-
-                    
-
+        //Добавлям в бд
         $email = new Emaillist();
         $email->email = $get['EMAIL'];
         $email->save();
+
+
+
+        Yii::$app->mailer->compose()
+            ->setFrom('vasa11514@gmail.com')
+            ->setTo('korotyailya1997@gmail.com')
+            ->setSubject('Уведемление с сайта ') // тема письма
+            ->setTextBody('Текстовая версия письма (без HTML)')
+            ->setHtmlBody('<p>HTML версия письма</p>')
+            ->send();
 
 
         return $this->render('index.php');
